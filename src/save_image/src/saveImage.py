bcc9ac80a71d90ed2d59441fee3e160433224629
CastielLiu <castiel_liu@outlook.com>
2019-06-08 15:26:27 +0800

    modify saveImage

--- src/save_image/src/saveImage.py
+++ src/save_image/src/saveImage.py
@@ -9,36 +9,35 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
 
-count = 0
 
 class SaveImage():
 	
-	def __init__(self,path):
+	def __init__(self,path,start_num):
 		rospy.init_node('save_image')
 		self.bridge = CvBridge()
 		self.sub_image = rospy.Subscriber("/image_rectified",Image,self.image_callback)
 		self.imagePath = path
+		self.start_num = start_num
 		
 	def run(self):
 		rospy.spin()
 		
 	def image_callback(self,image):
-		global count
 		try:
 			frame = self.bridge.imgmsg_to_cv2(image, "bgr8")
 		except CvBridgeError as e:
 			print(e)
-		file_name = self.imagePath + str(count)+'.jpg'
+		file_name = self.imagePath + str(self.start_num)+'.jpg'
 		status = cv2.imwrite(file_name, frame)
 		if status:
 			print(file_name+' saved')
 		else:
 			print('save image failed!!')
-		count = count+1
+		self.start_num = self.start_num+1
 		
 def main(argv):
 	if(len(argv)>1):
-		save_image = SaveImage(argv[1])
+		save_image = SaveImage(argv[1],int(argv[2]))
 		save_image.run()
 	
 if __name__ == '__main__':
