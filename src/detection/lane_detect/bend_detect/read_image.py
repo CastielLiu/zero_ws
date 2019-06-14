b4cbb85b7bff5ed8b98b7b5cb87c708e1a6cc674
spidersss <604815982@qq.com>
2019-06-14 21:28:03 +0800

    ..

--- src/detection/lane_detect/bend_detect/read_image.py
+++ src/detection/lane_detect/bend_detect/read_image.py
@@ -21,12 +21,32 @@ def publish_image(imgdata):
 	image_temp= bridge.cv2_to_imgmsg(imgdata,'bgr8')
 	
 	image_pubulish.publish(image_temp)
+
+def read_image(file_name):
+	img=cv2.imread(file_name)
+	img = cv2.resize(img,(640,480))
+	while True:
+		publish_image(img)
+		cv2.imshow('123',img)
+		key=cv2.waitKey(1000)
+		if key==ord('q'):
+			break
+		print("publish")
 	
-while True:
-	img=cv2.imread('bend.jpg')
-	publish_image(img)
-	cv2.imshow('123',img)
-	key=cv2.waitKey(1000)
-	if key==ord('q'):
-		break
-	print("pubulish")
+
+def read_video(video):
+	cap = cv2.VideoCapture(video)
+	cv2.namedWindow('srcImage')
+	while(cap.isOpened()):
+		ret,img = cap.read()
+		img = cv2.resize(img,(640,480))
+		publish_image(img)
+		cv2.imshow('srcImage',img)
+		key=cv2.waitKey(100)
+		if key==ord('q'):
+			break
+		print("publish")
+
+if __name__ == '__main__':
+	#read_video('/home/zwei/wendao/RoadLane_Detect(code_by_kevin)/test1.MOV')
+	read_image('bend.jpg')
