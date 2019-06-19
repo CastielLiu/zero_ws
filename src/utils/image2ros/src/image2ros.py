d3f8c4543452d1404ea822233f524490e3a7511b
spidersss <604815982@qq.com>
2019-06-19 23:56:56 +0800

    ..

---
+++ src/utils/image2ros/src/image2ros.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+import rospy
+import roslib
+import sys
+
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+import os
+import time
+
+class Image2ROS():
+	
+	def __init__(self,path):
+		rospy.init_node('image_to_ros')
+		self.bridge = CvBridge()
+		self.pub = rospy.Publisher("/image_rectified",Image,queue_size=1)
+		self.imagePath = path
+	def run(self):
+		image_names = os.listdir(self.imagePath)
+		
+		while not rospy.is_shutdown():
+			for image_name in image_names:
+				image_file = self.imagePath + '/' + image_name
+				
+				if os.path.isdir(image_file) or (image_file.split('.')[-1] != 'jpg') :
+					continue
+					
+				print('%s  published' %image_file)
+				
+				cv_image = cv2.imread(image_file)				
+				image_msg = self.bridge.cv2_to_imgmsg(cv_image, "bgr8")
+				self.pub.publish(image_msg)
+				time.sleep(0.05)
+				if rospy.is_shutdown():
+					return
+	
+		
+def main(argv):
+	if(len(argv)>1):
+		image2ros = Image2ROS(argv[1])
+		image2ros.run()
+	else:
+		print('please input the file path')
+	
+if __name__ == '__main__':
+	main(sys.argv)
+	
+
+
---
+++ src/utils/image2ros/src/video2ros.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+import rospy
+import roslib
+import sys
+
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+import os
+import time
+
+class Video2ROS():
+	
+	def __init__(self,path):
+		rospy.init_node('video_to_ros')
+		self.bridge = CvBridge()
+		self.pub = rospy.Publisher("/image_rectified",Image,queue_size=1)
+		self.videoPath = path
+	def run(self):
+		while True:
+			cap = cv2.VideoCapture(self.videoPath )
+			if cap is not None:
+				print('open %s ok.' %self.videoPath )
+			else:
+				print('open %s failed!' %self.videoPath )
+			while(cap.isOpened()):
+				ret,cv_image = cap.read()
+		
+				image_msg = self.bridge.cv2_to_imgmsg(cv_image, "bgr8")
+				self.pub.publish(image_msg)
+				time.sleep(0.05)
+				if rospy.is_shutdown():
+					return
+	
+		
+def main(argv):
+	if(len(argv)>1):
+		video2ros = Video2ROS(argv[1])
+		video2ros.run()
+	else:
+		print('please input the video path')
+	
+if __name__ == '__main__':
+	main(sys.argv)
+	
+
+
