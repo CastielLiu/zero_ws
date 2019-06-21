6dbb7f578fdb12ceb66bc886f2f423f2e88756ab
spidersss <604815982@qq.com>
2019-06-21 16:44:45 +0800

    add cameraInfo publisher in my_publisher

--- src/sensor/my_image_transport/src/my_publisher.cpp
+++ src/sensor/my_image_transport/src/my_publisher.cpp
@@ -4,52 +4,170 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/calib3d/calib3d.hpp"
 #include <cv_bridge/cv_bridge.h>
-#include <sstream> // for converting the command line parameter to integer
+#include "sensor_msgs/CameraInfo.h"
 
 #define _NODE_NAME_ "image_publisher"
 
-namespace ImageRectifier{
-
-bool Loadintrinsics(const std::string &calibration_file_path,
-					cv::Mat& camera_instrinsics, 
-					cv::Mat& distortion_coefficients)
+class ImageTalker
 {
-	if (calibration_file_path.empty())
+public:
+	ImageTalker()
 	{
-	    ROS_ERROR("[%s] missing calibration file path", _NODE_NAME_);
-	    return false;
+	}
+	void init(ros::NodeHandle& nh, ros::NodeHandle& nh_private,int cameraId)
+	{
+		camera_id_ = cameraId;
+		
+		image_transport::ImageTransport it(nh);
+	
+		std::string calibration_file_path;
+		nh_private.param<std::string>("calibration_file_path",calibration_file_path,"a.yaml");
+	
+		nh_private.param<int>("frame_rate",frame_rate_,30);
+	
+		ROS_INFO("%s",calibration_file_path.c_str());
+	
+		if(Loadintrinsics(calibration_file_path))
+		{
+			pub_ = it.advertise("/image_raw", 1);
+			is_rectify_ = false;
+		}
+		else
+		{
+			pub_ = it.advertise("/image_rectified", 1);
+			is_rectify_ = true;
+		}
+		camera_info_pub_ = nh.advertise<sensor_msgs::CameraInfo>("/camera_info", 1);
+		timer_ = nh.createTimer(ros::Duration(0.2), &ImageTalker::timerCallback, this);
+	}
+	
+	void timerCallback(const ros::TimerEvent& event)
+	{
+		this->pubCameraInfo();
+	}
+	
+	void run()
+	{
+		cv::VideoCapture cap(camera_id_);
+		
+		if(!cap.isOpened())
+		{
+			ROS_ERROR("can not open video device\n");
+			return;
+		}
+		
+		cv::Mat frame,src;
+		sensor_msgs::ImagePtr msg;
+
+		ros::Rate loop_rate(frame_rate_);
+		while (ros::ok())
+		{
+			cap >> frame;
+			//cv::flip(src,frame,-1); //将摄像头图像翻转一次再发布
+
+			if(!frame.empty()) 
+			{
+				if(is_rectify_)
+				{
+					cv::undistort(frame, src, camera_instrinsics_, distortion_coefficients_);
+					msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", src).toImageMsg();
+				}
+				else
+					msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
+				msg->header.frame_id="camera";
+				pub_.publish(msg);
+			}
+			loop_rate.sleep();
+			ros::spinOnce();
+		}
+	}
+	
+	bool Loadintrinsics(const std::string &calibration_file_path)
+	{
+		if (calibration_file_path.empty())
+		{
+			ROS_ERROR("[%s] missing calibration file path", _NODE_NAME_);
+			return false;
+		}
+
+		cv::FileStorage fs(calibration_file_path, cv::FileStorage::READ);
+
+		if (!fs.isOpened())
+		{
+			ROS_INFO("[%s] cannot open calibration file %s", _NODE_NAME_, calibration_file_path.c_str());
+	 		return false;
+		}
+
+		camera_instrinsics_ = cv::Mat(3, 3, CV_64F);
+		distortion_coefficients_ = cv::Mat(1, 5, CV_64F);
+
+		cv::Mat dis_tmp;
+		
+		fs["CameraMat"] >> camera_instrinsics_;
+		fs["DistCoeff"] >> dis_tmp;
+		fs["ImageSize"] >> imgSize_;
+		fs["DistModel"] >> distModel_;
+		
+		for (int col = 0; col < 5; col++)
+		{
+			distortion_coefficients_.at<double>(col) = dis_tmp.at<double>(col);
+		}
+		fs.release();	//释放
+		return true;
+	}
+	
+	void pubCameraInfo()
+	{
+		static bool instrinsics_parsed = false;
+		if (instrinsics_parsed)
+			goto PublishCameraInfo;
+		
+		for (int row = 0; row < 3; row++)
+			for (int col = 0; col < 3; col++)
+				camera_info_msg_.K[row * 3 + col] = camera_instrinsics_.at<double>(row, col);
+
+		for (int row = 0; row < 3; row++)
+			for (int col = 0; col < 4; col++)
+			{
+				if (col == 3)
+					camera_info_msg_.P[row * 4 + col] = 0.0f;
+				else
+					camera_info_msg_.P[row * 4 + col] = camera_instrinsics_.at<double>(row, col);
+			}
+		for (int row = 0; row < distortion_coefficients_.rows; row++)
+			for (int col = 0; col < distortion_coefficients_.cols; col++)
+				camera_info_msg_.D.push_back(distortion_coefficients_.at<double>(row, col));
+		camera_info_msg_.distortion_model = distModel_;
+		camera_info_msg_.height = imgSize_.height;
+		camera_info_msg_.width = imgSize_.width;
+		instrinsics_parsed = true;
+		camera_info_msg_.header.frame_id = "camera";
+	
+	PublishCameraInfo:
+		camera_info_msg_.header.stamp = ros::Time::now();
+		camera_info_pub_.publish(camera_info_msg_);
 	}
 
-	cv::FileStorage fs(calibration_file_path, cv::FileStorage::READ);
+private:
+	bool is_rectify_;
+	image_transport::Publisher pub_ ;
+	ros::Publisher camera_info_pub_;
+	ros::Timer timer_;
+	
+	cv::Mat camera_instrinsics_;
+	cv::Mat distortion_coefficients_;
+	std::string distModel_;
+	sensor_msgs::CameraInfo camera_info_msg_;
+	cv::Size imgSize_;
+	int camera_id_;
+	int frame_rate_;
+	
+};
 
-	if (!fs.isOpened())
-	{
-	    ROS_INFO("[%s] cannot open calibration file %s", _NODE_NAME_, calibration_file_path.c_str());
- 		return false;
-	}
-
-	camera_instrinsics = cv::Mat(3, 3, CV_64F);
-	distortion_coefficients = cv::Mat(1, 5, CV_64F);
-
-	cv::Mat dis_tmp;
-	fs["CameraMat"] >> camera_instrinsics;
-	fs["DistCoeff"] >> dis_tmp;
-	//fs["ImageSize"] >> image_size;
-
-	for (int col = 0; col < 5; col++)
-	{
-	    distortion_coefficients.at<double>(col) = dis_tmp.at<double>(col);
-	}
-	fs.release();	//释放
-	return true;
-}
-
-}
 
 int main(int argc, char** argv)
 {
-  // Check if video source has been passed as a parameter
-	if(argv[1] == NULL) 
+	if(argc <2) 
 	{
 		ROS_ERROR("argv[1]: camera number is empty\n");
 		return 1;
@@ -58,71 +176,11 @@ int main(int argc, char** argv)
 	ros::init(argc, argv, _NODE_NAME_);
 	ros::NodeHandle nh;
 	ros::NodeHandle nh_private("~");
-	image_transport::ImageTransport it(nh);
-	image_transport::Publisher pub ;
 	
-	std::string calibration_file_path;
-	nh_private.param<std::string>("calibration_file_path",calibration_file_path,"a.yaml");
+	int camera_id = atoi(argv[1]);
 	
-	int frame_rate_;
-	nh_private.param<int>("frame_rate",frame_rate_,30);
-	
-	ROS_INFO("%s",calibration_file_path.c_str());
-	
-	cv::Mat camera_instrinsics;
-	cv::Mat distortion_coefficients;
-	
-	bool is_rectify ;
-	if(!ImageRectifier::Loadintrinsics(calibration_file_path,camera_instrinsics,distortion_coefficients))
-	{
-		pub = it.advertise("/image_raw", 1);
-		is_rectify = false;
-	}
-	else
-	{
-		pub = it.advertise("/image_rectified", 1);
-		is_rectify = true;
-	}
-	
-	// Convert the passed as command line parameter index for the video device to an integer
-	std::istringstream video_sourceCmd(argv[1]);
-	int video_source;
-	// Check if it is indeed a number
-	if(!(video_sourceCmd >> video_source)) 
-	{
-		ROS_INFO("video_sourceCmd is %d\n",video_source);
-		return 1;
-	}
-
-	cv::VideoCapture cap(video_source);
-	// Check if video device can be opened with the given index
-	if(!cap.isOpened()) 
-	{
-		ROS_ERROR("can not open video device\n");
-		return 1;
-	}
-	
-	cv::Mat frame,src;
-	sensor_msgs::ImagePtr msg;
-
-	ros::Rate loop_rate(frame_rate_);
-	while (nh.ok())
-	{
-		cap >> frame;
-		//cv::flip(src,frame,-1); //将摄像头图像翻转一次再发布
-
-		if(!frame.empty()) 
-		{
-			if(is_rectify)
-			{
-				cv::undistort(frame, src, camera_instrinsics, distortion_coefficients);
-				msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", src).toImageMsg();
-			}
-			else
-				msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
-			msg->header.frame_id="camera";
-			pub.publish(msg);
-		}
-		loop_rate.sleep();
-	}
+	ImageTalker image_talker;
+	image_talker.init(nh,nh_private,camera_id);
+	image_talker.run();
+	return 0;
 }
