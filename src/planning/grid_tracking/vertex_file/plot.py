0fecf8e725988467682a510ae1754e8daa0d4f74
spidersss <604815982@qq.com>
2019-06-29 23:18:55 +0800

    ..

--- src/planning/grid_tracking/vertex_file/plot.py
+++ src/planning/grid_tracking/vertex_file/plot.py
@@ -2,10 +2,26 @@
 import matplotlib.pyplot as plt
 import math
 import sys
+import numpy as np
  
 g_longitudes = []
 g_latitudes =[]
 
+
+def BLH2XYZ(lon,lat,H=0.0):
+	B = lat*math.pi/180.0
+	L = lon*math.pi/180.0
+	a = 6378136.49
+	b = 6356755.00
+	e2 = (a*a-b*b)/(a*a)
+	sinB = math.sin(B)
+	cosB = math.cos(B)
+	N = a/math.sqrt(1-e2*sinB*sinB)
+	X = (N+H)*cosB*math.cos(L)
+	Y = (N+H)*cosB*math.sin(L)
+	Z = (N*(1-e2)+H)*sinB
+	return X,Y,Z
+	
 
 file_name = 'raw_path.txt'
 
@@ -20,6 +36,8 @@ with open(file_name,'r') as f:
 		g_latitudes.append(float(lat))
 
 
+
+plt.figure(1)
 plt.plot(g_longitudes,g_latitudes,'r-*',lw=1)
 
 plt.show()
