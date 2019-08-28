6a6073ef7a5ad7b4f3856b6a2cc66db71e49b313
CastielLiu <castiel_liu@outlook.com>
2019-08-28 21:43:34 +0800

    ..

--- src/utils/pcd2ros/src/pcd2ros_node.cpp
+++ src/utils/pcd2ros/src/pcd2ros_node.cpp
@@ -4,9 +4,33 @@
 #include <pcl/point_types.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/filters/impl/plane_clipper3D.hpp>
+#include <pcl/filters/extract_indices.h>
 
+using PointT = pcl::PointXYZI;
 
 ros::Publisher puba;
+
+pcl::PointCloud<PointT>::Ptr plane_clip(const pcl::PointCloud<PointT>::Ptr& src_cloud, const Eigen::Vector4f& plane, bool negative)
+{
+    pcl::PlaneClipper3D<PointT> clipper(plane);
+    pcl::PointIndices::Ptr indices(new pcl::PointIndices);
+
+    clipper.clipPointCloud3D(*src_cloud, indices->indices);
+
+    pcl::PointCloud<PointT>::Ptr dst_cloud(new pcl::PointCloud<PointT>);
+
+    pcl::ExtractIndices<PointT> extract;
+    extract.setInputCloud(src_cloud);
+    extract.setIndices(indices);
+    extract.setNegative(negative);
+    extract.filter(*dst_cloud);
+
+    return dst_cloud;
+}
+  
+  
 
 int main(int argc, char** argv) {
     // *****Initialize ROS
@@ -20,18 +44,30 @@ int main(int argc, char** argv) {
     }
  
     //*****load two pcd files
-    pcl::PointCloud<pcl::PointXYZ>::Ptr clouda_ptr (new pcl::PointCloud<pcl::PointXYZ>);//创建点云指针，存储点坐标xyz
-    if (pcl::io::loadPCDFile<pcl::PointXYZ> (argv[1], *clouda_ptr) == -1)
+    pcl::PointCloud<PointT>::Ptr cloud_raw (new pcl::PointCloud<PointT>);//创建点云指针，存储点坐标xyz
+    if (pcl::io::loadPCDFile<PointT> (argv[1], *cloud_raw) == -1)
     {
         PCL_ERROR ("Couldn't read file %s ^.^\n",argv[1]);
         return (-1);
     }
-    std::cerr << "size of a : " << clouda_ptr->width*clouda_ptr->height<< std::endl;
+    std::cerr << "size of a : " << cloud_raw->width*cloud_raw->height<< std::endl;
+    
+    pcl::PointCloud<PointT>::Ptr filtered(new pcl::PointCloud<PointT>);
+    float sensor_height = 1.8;
+    filtered = plane_clip(cloud_raw, Eigen::Vector4f(0.0f, 0.0f, 1.0f, sensor_height + 0.0), false);
+    filtered = plane_clip(cloud_raw, Eigen::Vector4f(0.0f, 0.0f, 1.0f, sensor_height - 1), true);
+    
+    boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+    
+	viewer->setBackgroundColor (0, 0, 0); 
+	viewer->addPointCloud<PointT> (filtered, "sample cloud");
+	viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "sample cloud");
+    viewer->addCoordinateSystem (1.0);
     
     //*****Convert the pcl/PointCloud data to sensor_msgs/PointCloud2
     sensor_msgs::PointCloud2 clouda_ros;
     
-    pcl::toROSMsg (*clouda_ptr, clouda_ros);
+    pcl::toROSMsg (*filtered, clouda_ros);
     
     std::string topic_name = "/point_cloud";
     std::string frame_id = "base_link";
@@ -47,6 +83,7 @@ int main(int argc, char** argv) {
 		clouda_ros.header.stamp = ros::Time::now();
 		clouda_ros.header.frame_id = frame_id;
 		puba.publish(clouda_ros);
+		viewer->spinOnce (100);
 		r.sleep();
     }
     return 0;
